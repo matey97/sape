22df587ee66d7f83fb13a1f82ae84bf3a170145a
al341802 <al341802@uji.es>
2018-03-23 12:33:13 +0100

    commit

---
+++ src/main/java/es/uji/ei1027/sape/SapeApplication.java
@@ -0,0 +1,12 @@
+package es.uji.ei1027.sape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SapeApplication.class, args);
+	}
+}
---
+++ src/test/java/es/uji/ei1027/sape/SapeApplicationTests.java
@@ -0,0 +1,16 @@
+package es.uji.ei1027.sape;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SapeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
