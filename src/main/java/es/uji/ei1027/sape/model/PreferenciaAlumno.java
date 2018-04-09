10932e97d9ea46f17844e5de44e5faef2a3e35b5
nachomestre <36193043+nachomestre@users.noreply.github.com>
2018-04-09 22:22:28 +0200

    Add files via upload

---
+++ src/main/java/es/uji/ei1027/sape/model/PreferenciaAlumno.java
@@ -0,0 +1,58 @@
+package es.uji.ei1027.sape.model;
+
+import java.sql.Date;
+
+public class PreferenciaAlumno {
+
+	int orden;
+	Date fechaUltimoCambio;
+	String estado;
+	String dni;
+	int numeroProyecto;
+
+	public int getOrden() {
+		return orden;
+	}
+
+	public void setOrden(int orden) {
+		this.orden = orden;
+	}
+
+	public Date getFechaUltimoCambio() {
+		return fechaUltimoCambio;
+	}
+
+	public void setFechaUltimoCambio(Date fechaUltimoCambio) {
+		this.fechaUltimoCambio = fechaUltimoCambio;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getDni() {
+		return dni;
+	}
+
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	public int getNumeroProyecto() {
+		return numeroProyecto;
+	}
+
+	public void setNumeroProyecto(int numeroProyecto) {
+		this.numeroProyecto = numeroProyecto;
+	}
+
+	@Override
+	public String toString() {
+		return "PreferenciaAlumno [orden=" + orden + ", fechaUltimoCambio=" + fechaUltimoCambio + ", estado=" + estado + ", dni=" + dni + ", numeroProyecto=" 
+				+ numeroProyecto + "]";
+	}
+}
